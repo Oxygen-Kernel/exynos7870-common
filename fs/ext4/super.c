526baa6cc219eb8e02e455fb219d8aa6f06d19be
DarkLord1731 <darklord1731@gmail.com>
2018-07-27 18:21:36 +0530

    ext4: Fix ext4_handle_error arguements

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -730,7 +730,7 @@ __acquires(bitlock)
 
 	ext4_unlock_group(sb, grp);
 	ext4_commit_super(sb, 1);
-	ext4_handle_error(sb);
+	ext4_handle_error(sb, page_buf);
 	/*
 	 * We only get here in the ERRORS_RO case; relocking the group
 	 * may be dangerous, but nothing bad will happen since the
